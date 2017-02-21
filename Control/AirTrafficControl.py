9cf761f1a51cdf747f7011226ad86abbdc394bac
Mark Raymond Jr <markrjr@live.com>
2017-02-21 14:14:22 -0600

    Implemented circular turning and flying at a target velocity.

--- Control/AirTrafficControl.py
+++ Control/AirTrafficControl.py
@@ -49,24 +49,30 @@ class Attitude(Attitude):
     return q
 
 class Tower(object):
-  LEVEL_ATTITUDE_BIT_FLAGS = 0b11100000
+  STANDARD_ATTITUDE_BIT_FLAGS = 0b11100000
   TURNING_ATTITUDE_BIT_FLAGS = 0b00000000
+  STANDARD_THRUST_CHANGE = 0.05
+  MAX_TURN_TIME = 5
   LAND_ALTITUDE = 0.5
   STANDARD_ATTITUDES = { 
                         "LEVEL" : Attitude(0,0,0),
                         "FORWARD" : Attitude(-5,0,0), # -5 degrees.
-                        "BACKWARD" : Attitude(5,0,0)  # +5 degrees.
+                        "BACKWARD" : Attitude(5,0,0),  # +5 degrees.
+                        "LEFT" : Attitude(-5, 0, -5),
+                        "RIGHT" : Attitude(-5, 0, 5)
                        }
-  STANDARD_SPEEDS = { 
+  STANDARD_THRUSTS = { 
                       "HOVER"      : 0.45,
-                      "LOW_SPEED"  : 0.50, b
+                      "LOW_SPEED"  : 0.50, 
                       "MED_SPEED"  : 0.55,
                       "HIGH_SPEED" : 0.60
                     }
                       
 
   def __init__(self):
-    self.start_time = datetime.now()
+    self.start_time = 0
+    self.last_thrust = 0
+    self.last_attitude = None
     self.system_initialized = False
     self.vehicle_initialized = False
 
@@ -96,6 +102,7 @@ class Tower(object):
       self.vehicle_state = "GROUND_IDLE"
       self.connected = True
       self.vehicle_initialized = True
+      self.start_time = datetime.now()
       self.last_attitude = [1, 0, 0, 0]
       print("Successfully connected to vehicle.")
 
@@ -107,27 +114,29 @@ class Tower(object):
     print("Building MAVLink message...")
     self.vehicle.mode = VehicleMode("STABILIZE")
     message = self.vehicle.message_factory.set_attitude_target_encode(
-      0,                        # Timestamp in milliseconds since system boot (not used).
-      0,                        # System ID
-      0,                        # Component ID
-      ATTITUDE_BIT_FLAGS,       # Bit flags. For more info, see http://mavlink.org/messages/common#SET_ATTITUDE_TARGET.
-      attitude.quaternion,      # Quaternions
-      0,                        # Body roll rate.
-      0,                        # Body pitch rate.
-      0,                        # Body yaw rate.
-      thrust                    # Collective thrust, from 0-1.
+      0,                                 # Timestamp in milliseconds since system boot (not used).
+      0,                                 # System ID
+      0,                                 # Component ID
+      STANDARD_ATTITUDE_BIT_FLAGS,       # Bit flags. For more info, see http://mavlink.org/messages/common#SET_ATTITUDE_TARGET.
+      attitude.quaternion,               # Quaternions
+      0,                                 # Body roll rate.
+      0,                                 # Body pitch rate.
+      0,                                 # Body yaw rate.
+      thrust                             # Collective thrust, from 0-1.
     )
     self.vehicle.send_mavlink(message)
     self.vehicle.flush()
     self.last_attitude = attitude
+    self.last_thrust = thrust
     print("Sent message.")
 
   def return_to_hover():
     self.vehicle_state = "HOVER"
-    set_thrust_angle_using_vectors(STANDARD_ATTITUDES["LEVEL"], STANDARD_SPEEDS["HOVER"])
+    set_thrust_angle_using_vectors(STANDARD_ATTITUDES["LEVEL"], STANDARD_THRUSTS["HOVER"])
+    self.vehicle.mode = VehicleMode("ALT HOLD")   
 
-  def takeoff_using_vectors(self, target_altitude, speed):
-    set_thrust_angle_using_vectors(STANDARD_ATTITUDES["LEVEL"], speed)
+  def takeoff_using_vectors(self, target_altitude, thrust):
+    set_thrust_angle_using_vectors(STANDARD_ATTITUDES["LEVEL"], thrust)
 
     while(vehicle.location.global_relative_frame.alt <= target_altitude):
       sleep(1)
@@ -135,10 +144,22 @@ class Tower(object):
     return_to_hover()
     print('Reached target altitude:{0:.2f}m'.format(vehicle.location.global_relative_frame.alt))
 
-  def fly_for_time(self, duration, direction, speed):
+  def fly_for_time(self, duration, direction, target_velocity):
     duration = timedelta(seconds=duration)
     end_manuever = datetime.now() + duration
-    set_thrust_angle_using_vectors(STANDARD_ATTITUDES["FORWARD"], speed)
+    set_thrust_angle_using_vectors(direction, self.last_thrust)
+    while(end_manuever <= datetime.now()):
+      if self.vehicle.velocity > target_velocity:
+        set_thrust_angle_using_vectors(direction, self.last_thrust - STANDARD_THRUST_CHANGE)
+      elif(self.vehicle.velocity < target_velocity):
+        set_thrust_angle_using_vectors(direction, self.last_thrust + STANDARD_THRUST_CHANGE)
+      sleep(1)
+    return_to_hover()
+
+  def fly_for_time_thrust(self, duration, direction, thrust):
+    duration = timedelta(seconds=duration)
+    end_manuever = datetime.now() + duration
+    set_thrust_angle_using_vectors(direction, thrust)
     while(end_manuever <= datetime.now()):
       sleep(1)
     return_to_hover()
@@ -150,6 +171,33 @@ class Tower(object):
     else:
       self.vehicle_state = "GROUND_IDLE"
 
-  def turn_in_place(self, angle):
-    set_thrust_angle_using_vectors(STANDARD_ATTITUDES["FORWARD"], speed)
+  def turn_for_time(self, direction, duration):
+    if duration > MAX_TURN_TIME:
+      return
+
+    print("Building MAVLink message...")
+
+    self.vehicle.mode = VehicleMode("ALT HOLD")
+
+    message = self.vehicle.message_factory.set_attitude_target_encode(
+      0,                                        # Timestamp in milliseconds since system boot (not used).
+      0,                                        # System ID
+      0,                                        # Component ID
+      TURNING_ATTITUDE_BIT_FLAGS,               # Bit flags. For more info, see http://mavlink.org/messages/common#SET_ATTITUDE_TARGET.
+      attitude.quaternion,                      # Attitude quaternion.
+      1,                                        # Body roll rate.
+      0,                                        # Body pitch rate.
+      1,                                        # Body yaw rate.
+      STANDARD_THRUSTS["LOW"]                   # Collective thrust, from 0-1.
+    )
+
+    self.vehicle.send_mavlink(message)
+    self.vehicle.flush()
+    self.last_attitude = attitude
+
+    print("Sent message.")
+
+    sleep(duration)
+
+    return_to_hover()
 
