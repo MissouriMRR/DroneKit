3423981fcba8addbc00442a267838e1ea14b516b
Mark Raymond Jr <markrjr@live.com>
2017-02-20 23:58:36 -0600

    Overrode attitude class from DroneKit.

--- Control/AirTrafficControl.py
+++ Control/AirTrafficControl.py
@@ -9,22 +9,57 @@
 # Lucas Coon, Mark Raymond Jr.
 
 from datetime import datetime, timedelta
-from dronekit import VehicleMode, connect
+from dronekit import VehicleMode, connect, Attitude
 from os import system
+from time import sleep
 
-import time
+import math
+
+class Attitude(Attitude):
+
+  def __init__(self, pitch, yaw, roll):
+    self.x = pitch
+    self.y = yaw
+    self.z = roll
+    self.pitch = math.radians(pitch)
+    self.yaw = math.radians(yaw)
+    self.roll = math.radians(roll)
+    self.quaternion = self.get_quaternion()
+
+  def get_quaternion(self):
+    q = []
+    
+    t0 = math.cos(self.yaw * 0.5)
+    t1 = math.sin(self.yaw * 0.5)
+    t2 = math.cos(self.roll * 0.5)
+    t3 = math.sin(self.roll * 0.5)
+    t4 = math.cos(self.pitch * 0.5)
+    t5 = math.sin(self.pitch * 0.5)
+
+    w = t0 * t2 * t4 + t1 * t3 * t5
+    x = t0 * t3 * t4 - t1 * t2 * t5
+    y = t0 * t2 * t5 + t1 * t3 * t4
+    z = t1 * t2 * t4 - t0 * t3 * t5
+
+    q.append(w)
+    q.append(x)
+    q.append(y)
+    q.append(z)
+
+    return q
 
 class Tower(object):
-  ATTITUDE_BIT_FLAGS = 0b11100000
+  LEVEL_ATTITUDE_BIT_FLAGS = 0b11100000
+  TURNING_ATTITUDE_BIT_FLAGS = 0b00000000
   LAND_ALTITUDE = 0.5
   STANDARD_ATTITUDES = { 
-                        "LEVEL" : [1, 0, 0, 0],
-                        "FORWARD" : [0.99905, 0, -0.04362, 0], # -5 degrees.
-                        "BACKWARD" : [0.99905, 0, 0.04362, 0]  # +5 degrees.
+                        "LEVEL" : Attitude(0,0,0),
+                        "FORWARD" : Attitude(-5,0,0), # -5 degrees.
+                        "BACKWARD" : Attitude(5,0,0)  # +5 degrees.
                        }
   STANDARD_SPEEDS = { 
                       "HOVER"      : 0.45,
-                      "LOW_SPEED"  : 0.50,
+                      "LOW_SPEED"  : 0.50, b
                       "MED_SPEED"  : 0.55,
                       "HIGH_SPEED" : 0.60
                     }
@@ -61,13 +96,14 @@ class Tower(object):
       self.vehicle_state = "GROUND_IDLE"
       self.connected = True
       self.vehicle_initialized = True
+      self.last_attitude = [1, 0, 0, 0]
       print("Successfully connected to vehicle.")
 
   def get_uptime(self):
     uptime = timedelta(self.start_time, datetime.now())
     return uptime
 
-  def set_thrust_angle_using_vectors(self, attitude_quaternion, thrust):
+  def set_thrust_angle_using_vectors(self, attitude, thrust):
     print("Building MAVLink message...")
     self.vehicle.mode = VehicleMode("STABILIZE")
     message = self.vehicle.message_factory.set_attitude_target_encode(
@@ -75,7 +111,7 @@ class Tower(object):
       0,                        # System ID
       0,                        # Component ID
       ATTITUDE_BIT_FLAGS,       # Bit flags. For more info, see http://mavlink.org/messages/common#SET_ATTITUDE_TARGET.
-      attitude_quaternion,      # Quaternions
+      attitude.quaternion,      # Quaternions
       0,                        # Body roll rate.
       0,                        # Body pitch rate.
       0,                        # Body yaw rate.
@@ -83,6 +119,7 @@ class Tower(object):
     )
     self.vehicle.send_mavlink(message)
     self.vehicle.flush()
+    self.last_attitude = attitude
     print("Sent message.")
 
   def return_to_hover():
@@ -93,26 +130,26 @@ class Tower(object):
     set_thrust_angle_using_vectors(STANDARD_ATTITUDES["LEVEL"], speed)
 
     while(vehicle.location.global_relative_frame.alt <= target_altitude):
-      time.sleep(1)
+      sleep(1)
 
     return_to_hover()
     print('Reached target altitude:{0:.2f}m'.format(vehicle.location.global_relative_frame.alt))
-    #After we reach the target altitude in meters, break out of the loop. 
-    #If you're above 1300 for a desired speed, you should ramp down to 1300 here as well.
 
   def fly_for_time(self, duration, direction, speed):
     duration = timedelta(seconds=duration)
     end_manuever = datetime.now() + duration
     set_thrust_angle_using_vectors(STANDARD_ATTITUDES["FORWARD"], speed)
     while(end_manuever <= datetime.now()):
-      time.sleep(1)
+      sleep(1)
     return_to_hover()
 
   def land(self):
     self.vehicle.mode = VehicleMode("LAND")
     while(vehicle.location.global_relative_frame.alt <= LAND_ALTITUDE):
-      time.sleep(1)
+      sleep(1)
     else:
       self.vehicle_state = "GROUND_IDLE"
 
+  def turn_in_place(self, angle):
+    set_thrust_angle_using_vectors(STANDARD_ATTITUDES["FORWARD"], speed)
 
