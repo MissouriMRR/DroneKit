0458f6c55dcdb3c3cf8531ffa2b6a0d512b93351
Mark Raymond Jr <markrjr@live.com>
2017-06-23 19:43:33 -0700

    GCS start.

---
+++ Control/GCS/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    socketio.run(app)
