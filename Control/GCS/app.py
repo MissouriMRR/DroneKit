d25ae41ef52e22ef3048f131ce70496755caa22b
Mark Raymond Jr <markrjr@live.com>
2017-07-16 12:55:53 -0700

    Implemented heartbeat thread, YAML based configs, and basic vehicle update structure. Other layout changes.

--- Control/GCS/app.py
+++ Control/GCS/app.py
@@ -1,7 +1,11 @@
 import sys
+import yaml
+import time
+from datetime import datetime
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
+from json import loads, dumps
 
 sys.path.append('../../Flight/')
 
@@ -13,16 +17,44 @@ socketio = SocketIO(app)
 
 tower = Tower()
 
+CONFIG_FILENAME = "drone_configs.yml"
+
+vehicle_config_data = None
+
+def load_config_file():
+    try:
+        global vehicle_config_data 
+        config_file = open(CONFIG_FILENAME, 'r')
+        vehicle_config_data = yaml.load(config_file)
+        config_file.close()
+    except IOError:
+        print("\nFailed to get configuration file, some information may not be available.\n")
+
 def get_battery():
-    return tower.vehicle.batter.voltage
+    battery_info = {}
+    battery_info["voltage"] = tower.vehicle.battery.voltage
+    battery_info["full_voltage"] = vehicle_config_data["battery"]["full_voltage"]
+    battery_info["percent_remaining"] = tower.vehicle.battery.voltage / vehicle_config_data["battery"]["full_voltage"]
+
+    return battery_info
+
+def fake_get_battery():
+    battery_info = {}
+    battery_info["voltage"] = 11.34
+    battery_info["full_voltage"] = vehicle_config_data["battery"]["full_voltage"]
+    battery_info["percent_remaining"] = 11.34 / vehicle_config_data["battery"]["full_voltage"]
+
+    return battery_info
 
 def get_vehicle_status():
     """Laundry list function.
 
     """
-    get_battery()
+    if not vehicle_config_data:
+        return
     status = {}
-    status['battery'] = get_battery()
+    status['battery'] = fake_get_battery()
+    status['hearbeat'] = int(time.mktime(datetime.utcnow().timetuple())) * 1000
     return status
 
 @app.route('/')
@@ -31,10 +63,22 @@ def index():
 
 @socketio.on('connect')
 def on_connect():
-    send('Initialization in progress...')
-    tower.initialize()
-    emit('status', {'status': get_vehicle_status()})
+    emit('information', 'Initialization in progress...')
+    load_config_file()
+    emit('vehicle_update', vehicle_config_data, json=True)
 
+@socketio.on('update_status')
+def on_status():
+    emit('status', get_vehicle_status())
+
+@socketio.on('initialization')
+def on_initialization(selected_vehicle_name):
+    global vehicle_config_data
+    for vehicle in vehicle_config_data:
+        if vehicle["name"] == selected_vehicle_name:
+            vehicle_config_data = vehicle
+            emit('status', get_vehicle_status())
+            return
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
