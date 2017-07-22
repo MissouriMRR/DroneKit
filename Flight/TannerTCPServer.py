f754b8737acfe4da4b2e53f30278ba646ae709b8
Tanner Winkelman <winkelmantanner@gmail.com>
2017-07-22 12:56:17 -0500

    Add files via upload
    Run these two python files in different terminal windows.  They send messages to each other.

---
+++ Flight/TannerTCPServer.py
@@ -0,0 +1,20 @@
+# start this first, then the client
+
+
+import socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('localhost', 8089))
+serversocket.listen(5) # become a server socket, maximum 5 connections
+
+connection, address = serversocket.accept()
+
+data = ""
+
+while data <> 'q' and data <> 'Q':
+    buf = connection.recv(64)
+    if len(buf) > 0:
+        print buf
+        data = raw_input ( "SEND( TYPE q or Q to Quit):" )
+        if(data <> 'q' and data <> 'Q'):
+            connection.send(data)
