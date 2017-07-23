a52b3d0a4d3001a3aacb6278d459eeb88c41296a
Innocent Niyibizi <25cent9@gmail.com>
2017-07-22 21:20:10 -0500

    Updated Scance
    Added ability to do one scan and then return that data to AirTrafficControl

--- Flight/Scanse.py
+++ Flight/Scanse.py
@@ -4,34 +4,42 @@ from sweeppy import Sweep
 
 
 class LIDAR():
+  MAX_SAFE_DISTANCE = 300.0
+  ANGLE_DIVISOR = 45.0
 
   def __init__(self):
     self.lidar_sensor = "/dev/ttyUSB0"
     self.sweep = None
-    self.enable_scanning = False
+    # self.enable_scanning = False
 
   def connect_to_lidar(self):
     self.sweep = Sweep(self.lidar_sensor)
     self.sweep.__enter__()
-    
+
+    self.sweep.set_motor_speed(2)
+    self.sweep.set_sample_rate(1000)
+
     speed = self.sweep.get_motor_speed()
     rate = self.sweep.get_sample_rate()
 
-    print('Motor Speed: {} Hz'.format(speed))
-    print('Sample Rate: {} Hz'.format(rate))
+    self.sweep.start_scanning()
 
   def get_lidar_data(self):
     # Starts scanning as soon as the motor is ready
-    self.sweep.start_scanning()
-    
     lidar_data = []
 
     # get_scans is coroutine-based generator lazily returning scans ad infinitum
-    for scan in itertools.islice(self.sweep.get_scans(), 1):
-      pass
+    for scan in itertools.islice(sweep.get_scans(), 1):
+        for sample in scan.samples:
+          distance = sample.distance
+          angle = math.radians(sample.angle / 1000.0) % 360.0
+        #   x = math.cos(angle) * distance
+        #   y = math.sin(angle) * distance
+          if distance < MAX_SAFE_DISTANCE:
+            lidar_data.append([distance, (angle%360.0)//ANGLE_DIVISOR])
 
     return lidar_data
-    
-  def shutdown(self):
-    self.sweep.__exit__()
 
+  def shutdown(self):
+    self.sweep.stop_scanning()
+    self.sweep.__exit__()
